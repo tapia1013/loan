70bc0f7f7c8f9367393fc9aa2411e144f8c322d6
tapia1013 <53988501+tapia1013@users.noreply.github.com>
2019-10-10 13:19:11 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,114 @@
+// Listen for submit
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+// Calculate Results
+function calculateResults(e) {
+  console.log('Calculating...');
+
+  // UI Vars
+  const amount = document.getElementById('amount');
+  const interest = document.getElementById('interest');
+  const years = document.getElementById('years');
+  const monthlyPayment = document.getElementById('monthly-payment');
+  const totalPayment = document.getElementById('total-payment');
+  const totalInterest = document.getElementById('total-interest');
+
+
+  // calculations
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
+
+
+  // Compute monthly payments
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+  // Check if monthly is a finite number
+  if (isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+
+    // total payment value
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+
+    // total interest
+    totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+  } else {
+    showError('Please check your numbers');
+  }
+
+
+  e.preventDefault();
+}
+
+
+// Show Error
+function showError(error) {
+  // create a div
+  const errorDiv = document.createElement('div');
+
+  // Insert to DOM after creating div and textNode/append
+  // Get Element
+  const card = document.querySelector('.card');
+  const heading = document.querySelector('.heading')
+
+  // Add class to div
+  errorDiv.className = 'alert alert-danger'
+
+  // Create textNode and append to div
+  errorDiv.appendChild(document.createTextNode(error))
+
+  // Insert error above heading
+  //pass in element u wanna put it, then put what insertB4
+  card.insertBefore(errorDiv, heading);
+
+  // Clear error after 3 sec with setTimeout
+  setTimeout(clearError, 3000);
+}
+
+// Create clearError
+function clearError() {
+  document.querySelector('.alert').remove();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
